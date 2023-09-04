205d31cb439096fdd70d8e6b34bddc3c2ad3359b
Andrea Torres <andrea.torres8087@alumnos.udg.mx>
2023-09-04 16:46:09 -0600

    creacion de formulario

---
+++ resources/views/contacto.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1> Formulario de contacto</h1>
+    <form action="" method= "POST">
+        <label for ="correo"> Correo: </label>
+        <label type ="email"> </label>
+        <textarea name="comentario" id="comentario" cols="20" rows = "10"> </textarea> 
+        <select name = "tipo" id="">
+            <option value = "alumno"> Alumno </option>
+            <option value = "empleado"> Empleado </option>
+</select>
+        <input type ="submit"> </input>
+</form>
+
+</body>
+</html>
